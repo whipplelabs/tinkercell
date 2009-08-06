7d433cb6236188c1a0cd59510ba235619fd5e33d
dchandran1 <deepakc@uw.edu>
2009-08-06 15:23:39 +0000

    trunk\DynamicCodeTools\DynamicCodeMain.cpp generates the C lib files using gcc (if Unix) and the tcc object files (if Win) for two most commonly used libraries.

--- DynamicCodeTools/DynamicCodeMain.cpp
+++ DynamicCodeTools/DynamicCodeMain.cpp
@@ -18,24 +18,51 @@ extern "C" MY_EXPORT void loadTCTool(Tinkercell::MainWindow * main)
 	QProcess proc;
     QString appDir = QCoreApplication::applicationDirPath();
 	QString homeDir = Tinkercell::MainWindow::userHome();
+	
+	proc.setWorkingDirectory(homeDir);
+	
+	//build the odesim and ssa libraries for use by various C plug-ins (windows: generate .o files, not libs)
+	
 #ifdef Q_WS_WIN
 
+	proc.start(QObject::tr("\"") + appDir + QObject::tr("\"\\win32\\tcc -r -I\"") + appDir + 
+				("\"/win32/include -I\"") + appDir + ("\"/c -L\"") + 
+				appDir + ("\"/win32/lib -o odesim.o \"") + 
+				appDir + QObject::tr("\"/c/cvode_src/cvode/*.c \"") + 
+				appDir + QObject::tr("\"/c/cvode_src/sundials/*.c \"") + 
+				appDir + QObject::tr("\"/c/cvode_src/nvec_ser/*.c \"") + 
+				appDir + QObject::tr("\"/c/cvodesim.c"));
+	proc.waitForFinished();
+	
 	proc.start(QObject::tr("\"") + appDir + QObject::tr("\"\\win32\\tcc -r -I\"") + 
 				appDir + ("\"/win32/include -I\"") + appDir + ("\"/c -L\"") + appDir + 
-				("\"/win32/lib -o \"") + homeDir + QObject::tr("\"/cells_ssa.o \"") + 
+				("\"/win32/lib -o cells_ssa.o \"") + 
 				appDir + QObject::tr("\"/c/mtrand.c \"") + 
 				appDir + QObject::tr("\"/c/ssa.c \"") + 
 				appDir + QObject::tr("\"/c/cells_ssa.c"));
 	proc.waitForFinished();
-	
-	proc.start(QObject::tr("\"") + appDir + QObject::tr("\"\\win32\\tcc -r -I\"") + appDir + 
-				("\"/win32/include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/win32/lib -o \"") + homeDir + 
-				QObject::tr("\"/odesim.o \"") + appDir + 
-				QObject::tr("\"/c/cvode_src/cvode/*.c \"") + appDir +
-				QObject::tr("\"/c/cvode_src/sundials/*.c \"") + appDir + 
-				QObject::tr("\"/c/cvode_src/nvec_ser/*.c \"") + appDir + 
-				QObject::tr("\"/c/cvodesim.c"));
+
+#else	//if not windows, assume gcc exists
+
+	proc.start(QObject::tr("gcc -c -o") + 
+				appDir + QObject::tr("/c/cvode_src/cvode/*.c ") +
+				appDir + QObject::tr("/c/cvode_src/sundials/*.c ") + 
+				appDir + QObject::tr("/c/cvode_src/nvec_ser/*.c ") + 
+				appDir + QObject::tr("/c/cvodesim.c"));
 	proc.waitForFinished();
+	
+	proc.start(QObject::tr("ar -r libodesim.a *.o"));
+	proc.waitForFinished();
+	
+	proc.start(QObject::tr("gcc -c -o ") +
+				appDir + QObject::tr("/c/mtrand.c \"") + 
+				appDir + QObject::tr("/c/ssa.c \"") + 
+				appDir + QObject::tr("/c/cells_ssa.c"));
+	proc.waitForFinished();
+	
+	proc.start(QObject::tr("ar -r libcells_ssa.a mtrand.o ssa.o cells_ssa.o"));
+	proc.waitForFinished();
+	
 #endif	
 	
 	Tinkercell::DynamicLibraryMenu * libMenu = new Tinkercell::DynamicLibraryMenu;
