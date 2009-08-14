01a97a20581319ea34bba930ffa6ad665f944718
dchandran1 <deepakc@uw.edu>
2009-08-14 19:03:04 +0000

    * updated the about box in bin and removed just redundant files

--- init.py
+++
@@ -1,7 +0,0 @@
-from numpy import *
-import scipy
-from pysces import *
-import networkx as nx
-from nxAutoLayout import *
-from tc2pysces import *
-print 'Python modules loaded: NetworkX, Numpy, Scipy, PySCeS'
