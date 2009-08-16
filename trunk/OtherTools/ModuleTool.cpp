ca6d3cdf94bee67d1a15e9646057859fdfeca9df
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2009-08-16 20:40:53 +0000

    * moduletool in progress still...

--- trunk/OtherTools/ModuleTool.cpp
+++ trunk/OtherTools/ModuleTool.cpp
@@ -132,6 +132,7 @@ namespace Tinkercell
                     alreadyLinked = true;
                     break;
                 }
+
                 if (alreadyLinked) continue;
 
                 NodeGraphicsItem * module = VisualTool::parentModule(items[i]);
