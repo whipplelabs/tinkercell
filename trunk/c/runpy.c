05129f06a01c4f1d83f94bb33d0a5a7d44e79515
arnaudgelas <arnaudgelas@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2009-08-22 12:10:18 +0000

    Fix for windows and add comment for mac case

--- trunk/c/runpy.c
+++ trunk/c/runpy.c
@@ -1,6 +1,8 @@
 #import <Python.h>
 #include "API/Python/TC_py.h"
-#include "dlfcn.h"
+//NOTE: (arnaudgelas)
+//on Mac the following line must be uncommented
+//#include "dlfcn.h"
 
 PyObject* main_dict;
 PyObject* dlfl_dict;
@@ -8,8 +10,13 @@ PyObject *errobj, *errdata, *errtraceback;
 
 void initialize()
 {
-	dlopen("/usr/lib/libpython2.5.dylib", RTLD_LAZY | RTLD_GLOBAL);	
-	Py_SetProgramName("Tinkercell");
+//  NOTE: (arnaudgelas)
+//  On Mac the 2 following lines must be uncommented
+//  Since the location/extension of this library is platform and machine
+//  dependent why not using CMake with something like @{PYTHON_LIBRARY}@
+//  or something like that???
+//	dlopen("/usr/lib/libpython2.5.dylib", RTLD_LAZY | RTLD_GLOBAL);	
+//	Py_SetProgramName("Tinkercell");
 	Py_Initialize();
 	
 	Py_InitModule("pytc", pytcMethods);
