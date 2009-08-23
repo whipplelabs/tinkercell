659d5354abdf1e1c1baecc3db4992284fb79b5e3
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2009-08-23 01:10:59 +0000

    * moved some CPack INSTALL commands to Main/CMakeLists.txt * added COMPONENT to some CPack INSTALL statements * modified the NSIS options.

--- trunk/CMake/TinkercellCPack.cmake
+++ trunk/CMake/TinkercellCPack.cmake
@@ -1,88 +1,18 @@
 #---------------------------------------------------------------------------
 # CMAKE Install files
 #---------------------------------------------------------------------------
-#
-# NOTE: (arnaudgelas)
-# that TINKERCELL_BINARY_BIN_DIR is EXECUTABLE_OUTPUT_PATH
-SET(TINKERCELL_BIN_DIR ${TINKERCELL_BINARY_DIR}/bin)
-
-
-#---------------------------------------------------------------------------
-#### NOTE: (arnaudgelas)
-#### The following has been moved in c/CMakeLists.txt.
-#### The best would be to do it when creating corresponding libraries.
-#### I have only move the following FILE( GLOB ... and INSTALL( ...
-#### for files in c subdirectories.
-####
-#FILE(GLOB cfiles "${TINKERCELL_SOURCE_DIR}/c/*.h" "${TINKERCELL_SOURCE_DIR}/c/*.c")
-#INSTALL(FILES ${cfiles} DESTINATION c)
-#
-#FILE(GLOB cvodeheaders "${TINKERCELL_SOURCE_DIR}/c/cvode/*.h")
-#INSTALL(FILES ${cvodeheaders} DESTINATION c/cvode)
-#
-#FILE(GLOB nvectorheaders "${TINKERCELL_SOURCE_DIR}/c/nvector/*.h")
-#INSTALL(FILES ${nvectorheaders} DESTINATION c/nvector)
-#
-#FILE(GLOB sundialsheaders "${TINKERCELL_SOURCE_DIR}/c/sundials/*.h")
-#INSTALL(FILES ${sundialsheaders} DESTINATION c/sundials)
-#
-#FILE(GLOB cvode_src_cvode "${TINKERCELL_SOURCE_DIR}/c/cvode_src/cvode/*.h" "${TINKERCELL_SOURCE_DIR}/c/cvode_src/cvode/*.c")
-#INSTALL(FILES ${cvode_src_cvode} DESTINATION c/cvode_src/cvode)
-#
-#FILE(GLOB cvode_src_cvode_fmix "${TINKERCELL_SOURCE_DIR}/c/cvode_src/cvode/fmix/*.h" "${TINKERCELL_SOURCE_DIR}/c/cvode_src/cvode/fmix/*.c")
-#INSTALL(FILES ${cvode_src_cvode_fmix} DESTINATION c/cvode_src/cvode/fmix)
-#
-#FILE(GLOB cvode_src_nvec_par "${TINKERCELL_SOURCE_DIR}/c/cvode_src/nvec_par/*.h" "${TINKERCELL_SOURCE_DIR}/c/cvode_src/nvec_par/*.c")
-#INSTALL(FILES ${cvode_src_nvec_par} DESTINATION c/cvode_src/nvec_par)
-#
-#FILE(GLOB cvode_src_sundials "${TINKERCELL_SOURCE_DIR}/c/cvode_src/sundials/*.h" "${TINKERCELL_SOURCE_DIR}/c/cvode_src/sundials/*.c")
-#INSTALL(FILES ${cvode_src_sundials} DESTINATION c/cvode_src/sundials)
-#
-#---------------------------------------------------------------------------
-
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/DB/Regulon/*.*" 
-  DESTINATION DB/Regulon
-  # COMPONENTS ??? #choose here a component
-)
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/NodeItems/*.*" 
-  DESTINATION NodeItems
-)
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/ArrowItems/*.*" 
-  DESTINATION ArrowItems
-)
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/OtherItems/*.*" 
-  DESTINATION OtherItems
-)
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/NodesTree/*.*" 
-  DESTINATION NodesTree
-)
-
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/py/*.*" 
-  DESTINATION py
-)
-
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/Plugins/*.*" 
-  DESTINATION Plugins
-)
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/Plugins/c/*.*" 
-  DESTINATION Plugins/c
-)
 
 ## NOTE: (arnaudgelas)
 # There is a special keyword for libraries...
-INSTALL(FILES "${TINKERCELL_BIN_DIR}/*.*" 
+INSTALL(FILES "${TINKERCELL_BINARY_BIN_DIR}/*.*" 
   DESTINATION .
 )
 
 IF(WIN32 AND NOT UNIX)
-   INSTALL(FILES "${TINKERCELL_BIN_DIR}/win32/*.*" DESTINATION win32)
-   INSTALL(FILES "${TINKERCELL_BIN_DIR}/win32/include/*.*" DESTINATION win32/include)
-   INSTALL(FILES "${TINKERCELL_BIN_DIR}/bin/win32/lib/*.*" DESTINATION win32/lib)
+   INSTALL(FILES "${TINKERCELL_BINARY_BIN_DIR}/win32/*.*" DESTINATION win32)
+   INSTALL(FILES "${TINKERCELL_BINARY_BIN_DIR}/win32/include/*.*" DESTINATION win32/include)
+   INSTALL(FILES "${TINKERCELL_BINARY_BIN_DIR}/win32/lib/*.*" DESTINATION win32/lib)
 ENDIF(WIN32 AND NOT UNIX)
-
-#INSTALL(TARGETS Tinkercell.exe RUNTIME DESTINATION .)
-
-#INSTALL(TARGETS NodeGraphics.exe RUNTIME DESTINATION .)
 
 
 # ---------------------
@@ -100,7 +30,7 @@ SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYRIGHT.TXT")
 SET(CPACK_PACKAGE_VERSION_MAJOR ${TINKERCELL_MAJOR_VERSION})
 SET(CPACK_PACKAGE_VERSION_MINOR ${TINKERCELL_MINOR_VERSION})
 IF(WIN32 AND NOT UNIX)
-  SET(CPACK_CMAKE_GENERATOR NSIS)
+  #SET(CPACK_CMAKE_GENERATOR NSIS)
   # There is a bug in NSI that does not handle full unix paths properly. Make
   # sure there is at least one set of four (4) backlasshes.
   SET(CPACK_NSIS_MUI_ICON "${TINKERCELL_SOURCE_DIR}/Main\\\\tinkercell.ico")
