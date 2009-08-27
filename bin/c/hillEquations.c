e38fa8db31249b811f53e3724d6c75d076a155a1
dchandran1 <deepakc@uw.edu>
2009-08-27 07:09:26 +0000

    * removed /bin... no longer needed

--- bin/c/hillEquations.c
+++
@@ -1,7 +0,0 @@
-#include "TC_api.h"
-
-int main()
-{
-    tc_runPythonFile("py/hillEquations.py");
-    return 0;
-}
--- bin/c/loadRegulonDB.c
+++
@@ -1,7 +0,0 @@
-#include "TC_api.h"
-
-int main()
-{
-    tc_runPythonFile("DB/Regulon/RegulonDB.py");
-    return 0;
-}
