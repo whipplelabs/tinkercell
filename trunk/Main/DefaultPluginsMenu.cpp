441aa2a16e19981ec47967e89944169a3c7749ca
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2009-09-30 22:20:36 +0000

    * layout options for history and console separately

--- trunk/Main/DefaultPluginsMenu.cpp
+++ trunk/Main/DefaultPluginsMenu.cpp
@@ -28,27 +28,77 @@ namespace Tinkercell
               SLOT(toolAboutToBeLoaded( Tool * , bool * )));
 			  
 	  
-	  toggleMainWindowLayoutAction = 0;
+	  toggleToolsLayoutAction = toggleHistoryLayoutAction = toggleConsoleLayoutAction = 0;
+	  
 	  if (main && main->settingsMenu)
 	  {
-		toggleMainWindowLayoutAction = main->settingsMenu->addAction(tr("Change Window Layout"));
-		connect(toggleMainWindowLayoutAction,SIGNAL(triggered()),this,SLOT(toggleMainWindowLayout()));
+			QMenu * menu = new QMenu(tr("Change layout"),main->settingsMenu);
+			main->settingsMenu->addMenu(menu);
+			
+			if (MainWindow::defaultToolWindowOption == MainWindow::ToolBoxWidget)			
+				toggleToolsLayoutAction = menu->addAction(tr("Use dock widgets for tools"));			
+			else			
+				toggleToolsLayoutAction = menu->addAction(tr("Use tool-box widgets for tools"));				
+			
+			if (MainWindow::defaultHistoryWindowOption == MainWindow::ToolBoxWidget)			
+				toggleHistoryLayoutAction = menu->addAction(tr("Use dock widgets for history"));			
+			else			
+				toggleHistoryLayoutAction = menu->addAction(tr("Use tool-box widgets for history"));
+				
+			if (MainWindow::defaultConsoleWindowOption == MainWindow::ToolBoxWidget)			
+				toggleConsoleLayoutAction = menu->addAction(tr("Use dock widgets for console"));			
+			else			
+				toggleConsoleLayoutAction = menu->addAction(tr("Use tool-box widgets for console"));
+
+			connect(toggleToolsLayoutAction,SIGNAL(triggered()),this,SLOT(toggleToolsLayout()));
+			connect(toggleHistoryLayoutAction,SIGNAL(triggered()),this,SLOT(toggleHistoryLayout()));
+			connect(toggleConsoleLayoutAction,SIGNAL(triggered()),this,SLOT(toggleConsoleLayout()));
 	  }
     }
 	
-	void DefaultPluginsMenu::toggleMainWindowLayout()
+	void DefaultPluginsMenu::toggleHistoryLayout()
+	{
+		if (MainWindow::defaultHistoryWindowOption == MainWindow::DockWidget)
+		{
+			toggleToolsLayoutAction->setText(tr("Use dock widgets for history"));
+			MainWindow::defaultHistoryWindowOption =  MainWindow::ToolBoxWidget;
+		}
+		else
+		{
+			toggleToolsLayoutAction->setText(tr("Use tool-box widgets for history"));
+			MainWindow::defaultHistoryWindowOption =  MainWindow::DockWidget;
+		}
+		QMessageBox::information(this,tr("History Window Layout"),tr("The change in layout will take effect the next time TinkerCell starts"));
+	}
+	
+	void DefaultPluginsMenu::toggleConsoleLayout()
+	{
+		if (MainWindow::defaultConsoleWindowOption == MainWindow::DockWidget)
+		{
+			toggleConsoleLayoutAction->setText(tr("Use dock widgets for console"));
+			MainWindow::defaultConsoleWindowOption =  MainWindow::ToolBoxWidget;
+		}
+		else
+		{
+			toggleConsoleLayoutAction->setText(tr("Use tool-box widgets for console"));
+			MainWindow::defaultConsoleWindowOption =  MainWindow::DockWidget;
+		}
+		QMessageBox::information(this,tr("Console Window Layout"),tr("The change in layout will take effect the next time TinkerCell starts"));
+	}
+	
+	void DefaultPluginsMenu::toggleToolsLayout()
 	{
 		if (MainWindow::defaultToolWindowOption == MainWindow::DockWidget)
 		{
-			toggleMainWindowLayoutAction->setText(tr("Use dock widgets"));
+			toggleToolsLayoutAction->setText(tr("Use dock widgets for tools"));
 			MainWindow::defaultToolWindowOption =  MainWindow::ToolBoxWidget;
 		}
 		else
 		{
-			toggleMainWindowLayoutAction->setText(tr("Use dock widgets"));
+			toggleToolsLayoutAction->setText(tr("Use tool-box widgets for tools"));
 			MainWindow::defaultToolWindowOption =  MainWindow::DockWidget;
 		}
-		QMessageBox::information(this,tr("Main Window Layout"),tr("The change in layout will take effect the next time TinkerCell starts"));
+		QMessageBox::information(this,tr("Tool Window Layout"),tr("The change in layout will take effect the next time TinkerCell starts"));
 	}
 
     DefaultPluginsMenu::~DefaultPluginsMenu()
