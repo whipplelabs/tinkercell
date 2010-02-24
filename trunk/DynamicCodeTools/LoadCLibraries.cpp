c8cddab2dfbbb7dbdf944f5c8f8435a658cd2556
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2010-02-24 19:34:02 +0000

    * MinGW (creative commons) replaces TCC (LGPL) due to compile issues * changed the classes in DynamicCodeTools to use ggcc instead o tcc

--- trunk/DynamicCodeTools/LoadCLibraries.cpp
+++ trunk/DynamicCodeTools/LoadCLibraries.cpp
@@ -295,7 +295,7 @@ namespace Tinkercell
 		appDir.replace(tr("/"),tr("\\"));
         proc.start(tr("del a.out"));
         proc.waitForFinished();
-		proc.start(tr("\"") + appDir + tr("\"\\win32\\tcc -I\"") + appDir + ("\"/win32/include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/c -L\"") + appDir + ("\"/win32/lib -r -w ") + filename  + tr(" -o a.out"));
+		proc.start(tr("\"") + appDir + tr("\"\\win32\\mingw\\bin\\gcc -I\"") + appDir + ("\"\\win32\\mingw\\include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/c -L\"") + appDir + ("\"\\win32\\mingw\\lib -r -w ") + filename  + tr(" -o a.out"));
         proc.waitForFinished();
         QString errors(proc.readAllStandardError());
         QString output(proc.readAllStandardOutput());
@@ -385,7 +385,7 @@ namespace Tinkercell
 		appDir.replace(tr("/"),tr("\\"));
 		userHome.replace(tr("/"),tr("\\"));
 
-        proc.start(tr("\"") + appDir + tr("\"\\win32\\tcc -I\"") + appDir + ("\"/win32/include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/c -L\"") + appDir + ("\"/win32/lib -w -shared -rdynamic ") + filename + tr(" -o ") + dllName + tr(".dll "));
+        proc.start(tr("\"") + appDir + tr("\"\\win32\\mingw\\bin\\gcc -I\"") + appDir + ("\"\\win32\\mingw\\include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/c -L\"") + appDir + ("\"\\win32\\mingw\\lib -w -shared -rdynamic ") + filename + tr(" -o ") + dllName + tr(".dll "));
         proc.waitForFinished();
         errors += (proc.readAllStandardError());
         output += tr("\n\n") + (proc.readAllStandardOutput());
@@ -468,7 +468,7 @@ namespace Tinkercell
 		appDir.replace(tr("/"),tr("\\"));
 		userHome.replace(tr("/"),tr("\\"));
 
-        proc.start(tr("\"") + appDir + tr("\"\\win32\\tcc -I\"") + appDir + ("\"/win32/include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/c -L\"") + appDir + ("\"/win32/lib -w -shared -rdynamic ") + filename + tr(" -o ") + dllName + tr(".dll "));
+        proc.start(tr("\"") + appDir + tr("\"\\win32\\mingw\\bin\\gcc -I\"") + appDir + ("\"\\win32\\mingw\\include -I\"") + appDir + ("\"/c -L\"") + appDir + ("\"/c -L\"") + appDir + ("\"\\win32\\mingw\\lib -w -shared -rdynamic ") + filename + tr(" -o ") + dllName + tr(".dll "));
         proc.waitForFinished();
         errors += (proc.readAllStandardError());
         output += tr("\n\n") + (proc.readAllStandardOutput());
