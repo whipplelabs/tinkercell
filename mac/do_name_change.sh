7ed20410d14e1b3411e0f4d8f7b070819054290a
dchandran1 <deepakc@uw.edu>
2010-03-05 03:24:09 +0000

    * complicated mac wasting my time

--- mac/do_name_change.sh
+++ mac/do_name_change.sh
@@ -5,6 +5,19 @@
 CURPATH = $1
 LIBFILES='*.dylib'
 PLUGINFILES='Plugins/*.dylib'
+CPLUGINFILES='Plugins/c/*.dylib'
+
+cp -R Plugins Tinkercell.app/Contents/MacOS/
+cp -R ../../c Tinkercell.app/Contents/MacOS/
+cp -R ../../ArrowItems Tinkercell.app/Contents/MacOS/
+cp -R ../../NodeItems Tinkercell.app/Contents/MacOS/
+cp -R ../../OtherItems Tinkercell.app/Contents/MacOS/
+mkdir Tinkercell.app/Contents/MacOS/NodesTree/
+cp ../../NodesTree/*.xml Tinkercell.app/Contents/MacOS/NodesTree/
+cp -R ../../NodesTree/Icons Tinkercell.app/Contents/MacOS/NodesTree/
+cp -R ../../py Tinkercell.app/Contents/MacOS/
+cp ../../py/python*.txt Tinkercell.app/Contents/MacOS/
+cp ../../*.txt Tinkercell.app/Contents/MacOS/
 
 for f in $LIBFILES
 do
@@ -17,23 +30,61 @@ done
 
 for f2 in $LIBFILES
 do
-    echo "install_name_tool Tinkercell ... $f2"
     install_name_tool \
           -change $CURPATH/$f2 \
-          $f2 \
+          @executable_path/../Frameworks/$f2 \
           Tinkercell.app/Contents/MacOS/Tinkercell
 done
 
+install_name_tool \
+          -change $CURPATH/libmuparser.dylib \
+          @executable_path/../Frameworks/libmuparser.dylib \
+          Tinkercell.app/Contents/Frameworks/libTinkerCellCore.dylib
+
 for f1 in $PLUGINFILES
 do
+  install_name_tool \
+          -id \
+          @executable_path/$f1 \
+          Tinkercell.app/Contents/MacOS/$f1
   for f2 in $LIBFILES
   do
-    echo "install_name_tool $f1 ... $f2"
+    echo "install_name_tool $CURPATH/$f2 ... Tinkercell.app/Contents/MacOS/$f1"
     install_name_tool \
           -change $CURPATH/$f2 \
-          $f2 \
+          @executable_path/../Frameworks/$f2 \
+          Tinkercell.app/Contents/MacOS/$f1
+  done
+  for f2 in $PLUGINFILES
+  do
+    echo "install_name_tool $CURPATH/$f2 ... Tinkercell.app/Contents/MacOS/$f1"
+    install_name_tool \
+          -change $CURPATH/$f2 \
+          @executable_path/$f2 \
+          Tinkercell.app/Contents/MacOS/$f1
+  done
+  for f2 in $CPLUGINFILES
+  do
+    echo "install_name_tool $CURPATH/$f2 ... Tinkercell.app/Contents/MacOS/$f1"
+    install_name_tool \
+          -change $CURPATH/$f2 \
+          @executable_path/$f2 \
           Tinkercell.app/Contents/MacOS/$f1
   done
 done
 
-
+for f1 in $CPLUGINFILES
+do
+  install_name_tool \
+          -id \
+          @executable_path/$f1 \
+          Tinkercell.app/Contents/MacOS/$f1  
+  for f2 in $CPLUGINFILES
+  do
+    echo "install_name_tool $CURPATH/$f2 ... Tinkercell.app/Contents/MacOS/$f1"
+    install_name_tool \
+          -change $CURPATH/$f2 \
+          @executable_path/$f2 \
+          Tinkercell.app/Contents/MacOS/$f1
+  done
+done
