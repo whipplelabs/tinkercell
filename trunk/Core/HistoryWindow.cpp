e6de07b54f688a1824cf439bee21ac04cbc4ac98
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2010-09-25 00:03:18 +0000

    * In the middle of some changes * DOES NOT COMPILE

--- trunk/Core/HistoryWindow.cpp
+++ trunk/Core/HistoryWindow.cpp
@@ -13,8 +13,59 @@ This is the history stack class that is used to store the undo/redo commands.
 namespace Tinkercell
 {
 
-	void HistoryWindow::undo() { stack()->undo(); }
-	void HistoryWindow::redo() { stack()->redo(); }
-	void HistoryWindow::push(QUndoCommand * command) { stack()->push(command); }
+	void HistoryWindow::HistoryWindow(bool b) : QUndoView()
+	{
+		useRestorePoints = useRestorePoints;
+		currentIndex = 0;
+		restorePoints << 0;
+	}
+
+	void HistoryWindow::undo() 
+	{ 
+		QUndoStack * s = stack();
+		if (s))
+		{
+			if (useRestorePoints && currentIndex > 0)
+			{
+				int d = restorePoints[currentIndex] - restorePoints[currentIndex-1];  //number of steps between restore points
+				for (int i=0; i < d; ++i)
+					s->undo();
+				--currentIndex;
+			}
+			else
+			{
+				s->undo(); 
+			}
+		}
+	}
+
+	void HistoryWindow::redo() 
+	{ 
+		QUndoStack * s = stack();
+		if (s))
+		{
+			if (useRestorePoints && currentIndex < (restorePoints.size()-1))
+			{
+				int d = restorePoints[currentIndex+1] - restorePoints[currentIndex];  //number of steps between restore points
+				for (int i=0; i < d; ++i)
+					s->redo();
+				++currentIndex;
+			}
+			else
+			{
+				s->redo(); 
+			}
+		}
+	}
+
+	void HistoryWindow::push(QUndoCommand * command) 
+	{ 
+		stack()->push(command);
+	}
+	
+	void setRestorePoint()
+	{
+		HistoryWindow::
+	}
 
 }
