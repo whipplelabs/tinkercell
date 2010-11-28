f04ea734a79cc2e7ee19f8dafd9d59c047cab792
dchandran1 <deepakc@uw.edu>
2010-11-28 02:28:20 +0000

    * working on C API for Copasi

--- ExternalCode/copasi/copasi_api.h
+++ ExternalCode/copasi/copasi_api.h
@@ -1,38 +1,57 @@
 #ifndef COPASI_SIMPLE_API
 #define COPASI_SIMPLE_API
 
+#include "TC_structs.h"
+
+/*!\brief this struct is used to contain a pointer to an instance of a COPASI class*/
+typedef struct  { 	void * CopasiModelPtr;  } copasi_model;
+
+/*!\brief this struct is used to contain a pointer to an instance of a COPASI class*/
+typedef struct  { 	void * CopasiSpeciesPtr;  void * CopasiModelPtr; } copasi_species;
+
+/*!\brief this struct is used to contain a pointer to an instance of a COPASI class*/
+typedef struct  { 	void * CopasiReactionPtr; void * CopasiModelPtr;  } copasi_reaction;
+
+/*!\brief this struct is used to contain a pointer to an instance of a COPASI class*/
+typedef struct  { 	void * CopasiCompartmentPtr; void * CopasiModelPtr;  } copasi_compartment;
+
+BEGIN_C_DECLS
+
 /*! 
  \brief initialize copasi -- MUST BE CALLED before calling any other functions
  \ingroup copasi
 */
-void copasi_init();
+TCAPIEXPORT void copasi_init();
 /*! 
  \brief destroy copasi -- MUST BE CALLED at the end of program
  \ingroup copasi
 */
-void copasi_end();
+TCAPIEXPORT void copasi_end();
 /*! 
  \brief create a model
  \param char* model name
  \return copasi_model a new model
  \ingroup copasi
 */
-copasi_model * createCopasiModel(const char* name);
+TCAPIEXPORT copasi_model createCopasiModel();
 /*! 
- \brief create a model
- \param char* model name
- \return copasi_model a new model
+ \brief add a compartment to the model
+ \param copasi_model* model
+ \param char* compartment name
+ \param double volume
+ \return copasi_species a new compartment
  \ingroup copasi
 */
-copasi_model * createCopasiModel(const char* name);
+TCAPIEXPORT copasi_compartment createCompartment(copasi_model model, const char* name, double volume);
 /*! 
  \brief add a species to the model
  \param copasi_model* model
  \param char* species name
+ \param double initial value (concentration or count, depending on the model)
  \return copasi_species a new species
  \ingroup copasi
 */
-copasi_species * createSpecies(copasi_model * model, const char* name);
+TCAPIEXPORT copasi_species createSpecies(copasi_compartment model, const char* name, double initial);
 /*! 
  \brief set a species as boundary or floating (will remove any assignment rules)
  \param copasi_model* model
@@ -40,14 +59,14 @@ copasi_species * createSpecies(copasi_model * model, const char* name);
  \param int boundary = 1, floating = 0 (default)
  \ingroup copasi
 */
-void setBoundarySpecies(copasi_species * species, int isBoundary);
+TCAPIEXPORT void setBoundarySpecies(copasi_species species, int isBoundary);
 /*! 
  \brief set the assignment rule for a species (automatically assumes boundary species)
  \param copasi_model* model
  \param char* formula, use 0 to remove assignment rule
  \ingroup copasi
 */
-void setAssignmentRule(copasi_species * species, const char * formula);
+TCAPIEXPORT void setAssignmentRule(copasi_species species, const char * formula);
 /*!
  \brief add a species or set an existing species as fixed
  \param copasi_model* model
@@ -55,7 +74,7 @@ void setAssignmentRule(copasi_species * species, const char * formula);
  \return copasi_reaction a new reaction
  \ingroup copasi
 */
-copasi_reaction * createReaction(copasi_model * model, const char* name);
+TCAPIEXPORT copasi_reaction createReaction(copasi_model model, const char* name);
 /*! 
  \brief add a reactant to a reaction
  \param copasi_reaction reaction
@@ -63,7 +82,7 @@ copasi_reaction * createReaction(copasi_model * model, const char* name);
  \param double stoichiometry
  \ingroup copasi
 */
-void addReactant(copasi_reaction * reaction, copasi_species * species, double stoichiometry);
+TCAPIEXPORT void addReactant(copasi_reaction reaction, copasi_species species, double stoichiometry);
 /*! 
  \brief add a product to a reaction
  \param copasi_reaction reaction
@@ -71,14 +90,14 @@ void addReactant(copasi_reaction * reaction, copasi_species * species, double st
  \param double stoichiometry
  \ingroup copasi
 */
-void addProduct(copasi_reaction * reaction, copasi_species * species, double stoichiometry);
+TCAPIEXPORT void addProduct(copasi_reaction reaction, copasi_species species, double stoichiometry);
 /*! 
  \brief set reaction rate equation
  \param copasi_reaction reaction
  \param char* formula
  \ingroup copasi
 */
-void setReactionRate(copasi_reaction * reaction, const char * formula);
+TCAPIEXPORT void setReactionRate(copasi_reaction reaction, const char * formula);
 
 /*! 
  \brief simulate using ODEs
@@ -89,7 +108,7 @@ void setReactionRate(copasi_reaction * reaction, const char * formula);
  \return tc_matrix matrix of concentration and/or flux values
  \ingroup copasi
 */
-tc_matrix simulateODE(copasi_model * model, double endtime, double dt, int returnConcOrFlux);
+TCAPIEXPORT tc_matrix simulateODE(copasi_model model, double endtime, double dt, int returnConcOrFlux);
 /*! 
  \brief simulate using Tau Leap (stochastic) algorithm
  \param copasi_model* model
@@ -99,7 +118,7 @@ tc_matrix simulateODE(copasi_model * model, double endtime, double dt, int retur
  \return tc_matrix matrix of concentration and/or flux values
  \ingroup copasi
 */
-tc_matrix simulateTauLeap(copasi_model * model, double endtime, double dt, int returnConcOrFlux);
+TCAPIEXPORT tc_matrix simulateTauLeap(copasi_model model, double endtime, double dt, int returnConcOrFlux);
 /*! 
  \brief simulate using exact stochastic method (Gillespie)
  \param copasi_model* model
@@ -108,7 +127,7 @@ tc_matrix simulateTauLeap(copasi_model * model, double endtime, double dt, int r
  \return tc_matrix matrix of concentration and/or flux values
  \ingroup copasi
 */
-tc_matrix simulateGillespie(copasi_model * model, double endtime, int returnConcOrFlux);
+TCAPIEXPORT tc_matrix simulateGillespie(copasi_model model, double endtime, int returnConcOrFlux);
 /*! 
  \brief get steady state
  \param copasi_model* model
@@ -118,7 +137,7 @@ tc_matrix simulateGillespie(copasi_model * model, double endtime, int returnConc
  \return tc_matrix matrix with parameter as the first column
  \ingroup copasi
 */
-tc_matrix parameterScan(copasi_model * model, const char * parameter, double startvalue, double endvalue);
+TCAPIEXPORT tc_matrix parameterScan(copasi_model model, const char * parameter, double startvalue, double endvalue);
 /*! 
  \brief get steady state
  \param copasi_model* model
@@ -128,8 +147,8 @@ tc_matrix parameterScan(copasi_model * model, const char * parameter, double sta
  \return tc_matrix matrix of concentration and flux values as the first row
  \ingroup copasi
 */
-tc_matrix getSteadyState(copasi_model * model, const char * parameter, double startvalue, double endvalue);
+TCAPIEXPORT tc_matrix getSteadyState(copasi_model model, const char * parameter, double startvalue, double endvalue);
 
-
+END_C_DECLS
 #endif
 
