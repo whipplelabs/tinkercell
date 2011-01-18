557b0a58deda5a4717a9b815c4510465a2e2a56c
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2011-01-18 23:49:20 +0000

    * linux automatic upload (sftp) using expect

---
+++ trunk/linux/upload.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd @TINKERCELL_BINARY_DIR@/_CPack_Packages/Linux/TGZ
+mv TinkerCell.tar.gz TinkerCell@TINKERCELL_BIT@.tar.gz
+
+/usr/bin/expect <<EOD
+set timeout -1
+
+spawn sftp dchandran1,tinkercell@frs.sourceforge.net
+expect *assword:
+
+send "May112008\r"
+expect sftp>
+
+send "cp /home/frs/project/t/ti/tinkercell\r"
+expect sftp>
+
+send "put TinkerCell*.tgz\r"
+expect sftp>
+
+send "exit\r"
+expect eof
+
+EOD
+
