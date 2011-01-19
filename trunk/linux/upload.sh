28f946b80e1ba10ea5b6f0f12400310da6225754
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2011-01-19 03:35:11 +0000

    * removed sourceforge account username too

--- trunk/linux/upload.sh
+++ trunk/linux/upload.sh
@@ -4,23 +4,28 @@ cd @TINKERCELL_BINARY_DIR@
 make package
 cd @TINKERCELL_BINARY_DIR@/_CPack_Packages/Linux/TGZ
 mv TinkerCell.tar.gz @TINKERCELL_EXE@@TINKERCELL_BIT@.tar.gz
+echo "renamed TinkerCell.tar.gz to @TINKERCELL_EXE@@TINKERCELL_BIT@.tar.gz"
 
 if [ @TINKERCELL_UPLOAD_SOURCE@ == ON ]
   then
-    echo "creating source tarball..."
     cd @TINKERCELL_SOURCE_DIR@
     cd ..
-   svn export TinkerCell TinkerCellSource
-    tar czvf @TINKERCELL_BINARY_DIR@/_CPack_Packages/Linux/TGZ/TinkerCellSource.tar.gz TinkerCellSource
+	echo "extracting source code..."
+    svn export TinkerCell TinkerCellSource
+    echo "compressing source code..."
+    tar czf @TINKERCELL_BINARY_DIR@/_CPack_Packages/Linux/TGZ/TinkerCellSource.tar.gz TinkerCellSource
     rm -Rf TinkerCellSource
+	echo "created @TINKERCELL_BINARY_DIR@/_CPack_Packages/Linux/TGZ/TinkerCellSource.tar.gz"
 fi 
+
+echo "logging in to Sourceforge as @TINKERCELL_SOURCEFORGE_USERNAME@"
 
 cd @TINKERCELL_BINARY_DIR@/_CPack_Packages/Linux/TGZ
 
 /usr/bin/expect <<EOD
 set timeout -1
 
-spawn sftp dchandran1,tinkercell@frs.sourceforge.net
+spawn sftp @TINKERCELL_SOURCEFORGE_USERNAME@,tinkercell@frs.sourceforge.net
 expect *assword:
 
 send "@TINKERCELL_SOURCEFORGE_PASSWORD@\r"
