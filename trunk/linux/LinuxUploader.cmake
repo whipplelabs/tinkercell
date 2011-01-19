623f18e2d76c1f85bd05d9c2bd86e6e35cc2675c
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2011-01-19 02:51:05 +0000

    * removed sourceforge password from scripts and made it a variable obtained from CMake (user entered)

--- trunk/linux/LinuxUploader.cmake
+++ trunk/linux/LinuxUploader.cmake
@@ -9,19 +9,26 @@ ELSE(TINKERCELL_64BIT)
      SET(TINKERCELL_BIT 32)
 ENDIF(TINKERCELL_64BIT)
 
-CONFIGURE_FILE( 
-		${TINKERCELL_SOURCE_DIR}/linux/run_tinkercell.sh
-		${TINKERCELL_BINARY_DIR}/run_tinkercell.sh
-		@ONLY
-	)
+IF (DEFINED TINKERCELL_SOURCEFORGE_PASSWORD)
 
-CONFIGURE_FILE( 
-		${TINKERCELL_SOURCE_DIR}/linux/upload.sh
-		${TINKERCELL_BINARY_DIR}/upload.sh
-		@ONLY
-	)
+	CONFIGURE_FILE( 
+			${TINKERCELL_SOURCE_DIR}/linux/run_tinkercell.sh
+			${TINKERCELL_BINARY_DIR}/run_tinkercell.sh
+			@ONLY
+		)
 
-MESSAGE("To run TinkerCell, source ${TINKERCELL_BINARY_DIR}/run_tinkercell.sh")
-MESSAGE("To upload TinkerCell, source ${TINKERCELL_BINARY_DIR}/upload.sh")
+	CONFIGURE_FILE( 
+			${TINKERCELL_SOURCE_DIR}/linux/upload.sh
+			${TINKERCELL_BINARY_DIR}/upload.sh
+			@ONLY
+		)
 
+	MESSAGE("To upload TinkerCell, source ${TINKERCELL_BINARY_DIR}/upload.sh")
+ELSE (DEFINED TINKERCELL_SOURCEFORGE_PASSWORD)
+	
+	MESSAGE( "Please create a New Entry of type STRING called TINKERCELL_SOURCEFORGE_PASSWORD and enter your Sourceforge password")
+	
+ENDIF (DEFINED TINKERCELL_SOURCEFORGE_PASSWORD)
+
+MESSAGE("To run TinkerCell, source ${TINKERCELL_BINARY_DIR}/run_tinkercell.sh")	
 INSTALL(FILES ${TINKERCELL_BINARY_DIR}/linux/run_tinkercell.sh DESTINATION bin/../ COMPONENT "TinkerCell")
