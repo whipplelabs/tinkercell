1b4483f9b14c346508ccf5bb8a553f0245b481ae
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2011-03-10 04:14:15 +0000

    tryAllModules works but slow

--- trunk/python/moduleSwapper.py
+++ trunk/python/moduleSwapper.py
@@ -1,6 +1,6 @@
 """
 category: Simulation
-name: Module Swap
+name: Model generator
 descr: A convenient way to see the simulation result due to the affect of different submodels
 menu: yes
 icon: module.png
@@ -12,12 +12,14 @@ listOfModules = []
 allItems = tc_allItems()
 homeDir = tc_homeDir() + "/Modules/"
 hsz = len(homeDir)
+time = tc_getNumber("Simulation time")
 
 for i in range(0, allItems.length):
     item = tc_getItem(allItems, i)
     submodels = tc_listOfPossibleModels(item)
     if submodels.length > 0 and tc_getParent(item) == 0:
         listOfModules.append(item)
+        print tc_getUniqueName(item) + " : " + str(submodels.length)
 
 inputWindow = tc_createMatrix( len(listOfModules), 1 )
 for i in range( 0, len(listOfModules) ):
@@ -29,6 +31,7 @@ for i in range( 0, len(listOfModules) ):
     options = tc_listOfPossibleModels( listOfModules[i] )
     for j in range(0, options.length):
         s = tc_getString(options, j)
+        print s
         if s[0:hsz] == homeDir:
             n = len(s)
             s = s[hsz:n]
@@ -39,6 +42,7 @@ for i in range( 0, len(listOfModules) ):
 
 def substituteSubModel(*arg):
     global listOfModules
+    tc_enableClustering(4)
     homeDir = tc_homeDir() + "/Modules/"
     n = len(listOfModules)
     if len(arg) < n:
@@ -50,6 +54,6 @@ def substituteSubModel(*arg):
         s = homeDir + s
         tc_substituteModel(listOfModules[i], s)
 
-    m = tc_simulateDeterministic(0,100,200)
+    m = tc_simulateDeterministic(0,time,100)
     tc_plot(m, "simulation")
 
