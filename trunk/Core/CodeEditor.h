c14bfe81d42480774d2310d2dfa7a489af55a8b6
dchandran1 <dchandran1@95d75fa2-39cf-44e0-8ea2-df6a562dc58c>
2011-04-12 19:48:49 +0000

    * find dialog stuff

--- trunk/Core/CodeEditor.h
+++ trunk/Core/CodeEditor.h
@@ -1,50 +1,14 @@
 /****************************************************************************
-** This file is a combination of two example programs included in the Qt Toolkit.
-** Below is the Qt copyright information as included in the example programs:
-**
-** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
-** Contact: Qt Software Information (qt-info@nokia.com)
-**
-** This file is part of the example classes of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial Usage
-** Licensees holding valid Qt Commercial licenses may use this file in
-** accordance with the Qt Commercial License Agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Nokia.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain
-** additional rights. These rights are described in the Nokia Qt LGPL
-** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
-** package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-** If you are unsure which license is appropriate for your use, please
-** contact the sales department at qt-sales@nokia.com.
-** $QT_END_LICENSE$
-**
+This file is a combination of two example programs included in the Qt Toolkit.
+A few modifications have been added, but the majority of the code is from Qt's
+demo programs
 ****************************************************************************/
-
-#ifndef QT_DEMO_COMPLETER_AND_CODEEDITOR_H
-#define QT_DEMO_COMPLETER_AND_CODEEDITOR_H
+#ifndef QT_MODIEIFED_DEMO_COMPLETER_AND_CODEEDITOR_H
+#define QT_MODIEIFED_DEMO_COMPLETER_AND_CODEEDITOR_H
 
 #include <QPlainTextEdit>
+#include <QDialog>
+#include <QLineEdit>
 #include <QCompleter>
 #include <QKeyEvent>
 #include <QAbstractItemView>
@@ -92,6 +56,7 @@ namespace Tinkercell
 	
 	public slots:
 		void setText(const QString&);
+		void showFindReplaceDialog();
 		bool find(const QString&);
 		bool replace(const QString&,const QString&);
 
@@ -106,11 +71,14 @@ namespace Tinkercell
 		void highlightCurrentLine();
 		void updateLineNumberArea(const QRect &, int);
 		void insertCompletion(const QString &completion);
+		void find();
+		void replace();
 
 	private:
 		QString textUnderCursor() const;
-
-	private:
+		QDialog * findReplaceDialog;
+		QLineEdit * findLineEdit;
+		QLineEdit * replaceLineEdit;
 		QCompleter *c;
 
 	};
@@ -136,7 +104,7 @@ namespace Tinkercell
 		}
 
 	private:
-		CodeEditor *codeEditor;
+		CodeEditor * codeEditor;
 	};
 
 }
