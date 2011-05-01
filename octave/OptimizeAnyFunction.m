e727d8501434e188bc07e9184847ebbaa391aca4
dchandran1 <deepakc@uw.edu>
2011-05-01 05:16:44 +0000

    * added optimize functions for Py and Oc (not tested yet) * added toTC and fromTC hach into octave interpreter

---
+++ octave/OptimizeAnyFunction.m
@@ -0,0 +1,35 @@
+#category: Optimization
+#name: Optimize any Octave function
+#description: Write a function and get the distribution of parameters that satisfies that function
+#icon: octave.png
+#menu: yes
+#tool: no
+
+fout = fopen("example.m","w");
+fprintf(fout,"#Copy this example code into the Coding Window in TinkerCell.\n\
+#Edit the Objective function and run the code.\n\
+#objective function for CrossEntropy\n\
+function y = Objective()\n\
+    ss = tinkercell.tc_getSteadyState();\n\
+    y = randn(0,1);\n\
+end\n\
+#minimize or maximize?\n\
+minimize = 0;\n\
+res = CrossEntropy.OptimizeParameters(@Objective, \"name of function\", 100, 100, minimize);\n\
+#get the results and analyze results\n\
+mu = res(0);\n\
+sigma2 = res(1);\n\
+paramnames = res(2);\n\
+CrossEntropy.DoPCA(mu, sigma2, paramnames);\n\
+#now we set the parameters back in the model (option)\n\
+n = size(mu,1);\n\
+params = tinkercell.tc_createMatrix(n, 1);\n\
+for i = 1:n\n\
+    tinkercell.tc_setMatrixValue(params, i, 0, mu(i));\n\
+    tinkercell.tc_setRowName(params, i, paramnames(i));\n\
+end\n\
+tinkercell.tc_setParameters(params,1);\n\
+");
+fclose(fout);
+tinkercell.tc_openUrl("example.m")
+
---
+++ python/OptimizeAnyFunction.py
@@ -0,0 +1,39 @@
+"""
+category: Optimization
+name: Optimize any Python function
+description: Write a function and get the distribution of parameters that satisfies that function
+icon: function.png
+menu: yes
+tool: no
+"""
+fout = open("example.py","w")
+s = "#Copy this example code into the Coding Window in TinkerCell.\n\
+#Edit the Objective function and run the code.\n\
+from tinkercell import *\n\
+from tc2py import *\n\
+from numpy import *\n\
+import CrossEntropy\n\
+#objective function for CrossEntropy\n\
+def Objective():\n\
+    ss = tc_getSteadyState()\n\
+    return (numpy.random.normal(0,1))\n\
+#minimize or maximize?\n\
+minimize = False\n\
+res = CrossEntropy.OptimizeParameters(@Objective, \"name of function\", 100, 100, minimize)\n\
+#get the results and analyze results\n\
+mu = res[0]\n\
+sigma2 = res[1]\n\
+paramnames = res[2]\n\
+CrossEntropy.DoPCA(mu, sigma2, paramnames)\n\
+#now we set the parameters back in the model (option)\n\
+n = len(mu)\n\
+params = tc_createMatrix(n, 1)\n\
+for i in range(0,n):\n\
+    tc_setMatrixValue(params, i, 0, mu[i])\n\
+    tc_setRowName(params, i, paramnames[i])\n\
+tc_setParameters(params,1)\n\
+"
+fout.write(s)
+fout.close()
+tc_openUrl("example.py")
+
